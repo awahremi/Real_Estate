ebb08be405ddd24c9c9c21ac96c708f4c774d875
unknown <awahremi@gmail.com>
2022-12-06 23:19:25 -0800

    Third commit

--- frontend/src/Pages/login.js
+++ frontend/src/Pages/login.js
@@ -1,7 +1,47 @@
-const Login= () =>{
-    return (
-        <div>Login page</div>
-    )
+import React from "react";
+import "./login.css"
+
+const LoginForm = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email);
+    console.log(password);
+    setPassword("");
+    setEmail("");
+  };
+
+  return (
+    <div className="input-form">
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        name="email"
+        value={email}
+        placeholder="Enter email"
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
+      />
+      <br />
+      <br />
+      <input
+        type="password"
+        name="password"
+        value={password}
+        placeholder="Enter password"
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
+      <br />
+      <br />
+      <button type="submit">Login</button>
+    </form>
+    </div>
+  );
 };
 
-export default Login;
+export default LoginForm;
